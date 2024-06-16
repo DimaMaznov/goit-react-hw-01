01bbd7db652dc28049e3f83c12ecfa816a5a22b9
DimaMaznov <maznovdmitriy01@gmail.com>
2024-06-16 23:17:10 +0300

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,27 +1,27 @@
 /* eslint-disable no-irregular-whitespace */
-const Profile = ({name,tag,location ,image, stats}) => { return (<div>
+const Profile = ({name,tag,location,avatar, stats}) => { return (<div>
     <div>
       <img
-        src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+        src= {avatar}
         alt="User avatar"
       />
-      <p>Petra Marica</p>
-      <p>@pmarica</p>
-      <p>Salvador, Brasil</p>
+      <p>{name}</p>
+      <p>@{tag}</p>
+      <p>{location}</p>
     </div>
   
     <ul>
       <li>
         <span>Followers</span>
-        <span>1000</span>
+        <span>{stats.followers}</span>
       </li>
       <li>
         <span>Views</span>
-        <span>2000</span>
+        <span>{stats.views}</span>
       </li>
       <li>
         <span>Likes</span>
-        <span>3000</span>
+        <span>{stats.likes}</span>
       </li>
     </ul>
   </div>
