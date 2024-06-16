5624de904f0b7afd9dd1242c64229ef413b14b50
DimaMaznov <maznovdmitriy01@gmail.com>
2024-06-16 20:43:13 +0300

    final

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,11 @@
 /* eslint-disable no-irregular-whitespace */
 import Profile from "../Profile/Profile";
-import userData from "../userData.json";
+import FriendList from "../FriendList/Friendlist";
+import TransactionHistory from "../TransactionHistory/TransactionHistory";
+import userData from "../../userData.json";
+import friends from "../../friends.json";
+import transactions from "../../transactions.json";
+
     
     
     const App = () => {
@@ -13,6 +18,8 @@ import userData from "../userData.json";
             image={userData.avatar}
             stats={userData.stats}
           />
+    <FriendList friends={friends} />
+    <TransactionHistory items={transactions} />
         </>
       );
     };
